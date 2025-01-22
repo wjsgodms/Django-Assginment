51a5e2e5649ba89961b9d4a36f0eafbf50023791
전해은 <ggoo7731@naver.com>
2025-01-22 13:30:46 +0900

    ✨feat: chapter_03

--- config/urls.py
+++ config/urls.py
@@ -17,14 +17,21 @@ Including another URLconf
 
 # config/urls.py
 
-from django.contrib import admin
-from django.urls import path
+# config/urls.py
 
-from todo.views import todo_list, todo_info  # 추가
+from django.contrib import admin
+from django.urls import path, include
+from todo.views import todo_list, todo_info
+from users import views as user_views
 
 urlpatterns = [
+    path('todo/', todo_list, name='todo_list'),
+    path('todo/<int:todo_id>/', todo_info, name='todo_info'),
     path('admin/', admin.site.urls),
+    path('accounts/', include('django.contrib.auth.urls')),
+    path('accounts/login/', users_views.login, name='login'),
+    path('accounts/signup/', users_views.sign_up, name='signup')
+]
 
-    path('todo/', todo_list, name='todo_list'),  # 추가
-    path('todo/<int:todo_id>/', todo_info, name='todo_info'),  # 추가
+
 ]
